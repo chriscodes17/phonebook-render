6e87640b41e086aeaa176e716d387d9f20bc9256
Chris Juarez <chrisjuarez@Chriss-MacBook-Air.local>
2023-04-16 17:38:56 -0700

    add process env port

--- index.js
+++ index.js
@@ -95,4 +95,7 @@ app.post("/api/persons", (req, res) => {
   res.json(person);
 });
 
-app.listen(3001, () => console.log("Port 3001"));
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
