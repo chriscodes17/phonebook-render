f00d86f998e71ddb2801fe57bcfbf372fb2b40d5
Chris Juarez <chrisjuarez@Chriss-MacBook-Air.local>
2023-04-20 17:50:55 -0700

    added error handler and updated routes to handle mongodb

--- index.js
+++ index.js
@@ -6,28 +6,13 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static("build"));
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+const errorHandler = (error, req, res, next) => {
+  console.log(error);
+  if (error.name === "CastError") {
+    res.status(400).send({ error: "Malformatted id" });
+  }
+  next(error);
+};
 
 app.get("/api/persons", (req, res) => {
   Person.find({}).then((persons) => {
@@ -46,41 +31,72 @@ app.get("/info", (req, res) => {
   });
 });
 
-app.get("/api/persons/:id", (req, res) => {
+app.get("/api/persons/:id", (req, res, next) => {
   const id = req.params.id;
-  Person.findById(id).then((person) => {
-    res.json(person);
-  });
+  Person.findById(id)
+    .then((person) => {
+      res.json(person);
+    })
+    .catch((error) => {
+      return next(error);
+    });
 });
 
-app.delete("/api/persons/:id", (req, res) => {
-  const id = +req.params.id;
-  persons = persons.filter((p) => p.id !== id);
-  res.status(204).end();
+app.delete("/api/persons/:id", (req, res, next) => {
+  const id = req.params.id;
+  Person.findByIdAndRemove(id)
+    .then((result) => {
+      res.status(204).end();
+    })
+    .catch((error) => {
+      return next(error);
+    });
 });
 
-app.post("/api/persons", (req, res) => {
+app.post("/api/persons", async (req, res, next) => {
+  try {
+    const body = req.body;
+    if (!body.name || !body.number) {
+      return res.status(400).json({
+        error: "name or number is missing, please fill in all required fields",
+      });
+    }
+    let persons = await Person.find({});
+    const checkName = persons.find((p) => p.name === body.name) ? true : false;
+    if (checkName) {
+      return res.status(400).json({
+        error:
+          "name already exists in the phonebook, please use a different name",
+      });
+    }
+    const person = new Person({
+      name: body.name,
+      number: body.number,
+    });
+    const savedPerson = await person.save();
+    res.json(savedPerson);
+  } catch (error) {
+    return next(error);
+  }
+});
+
+app.put("/api/persons/:id", (req, res, next) => {
+  const id = req.params.id;
   const body = req.body;
-  const checkName = persons.find((p) => p.name === body.name) ? true : false;
-  if (!body.name || !body.number) {
-    return res.status(400).json({
-      error: "name or number is missing, please fill in all required fields",
-    });
-  }
-  if (checkName) {
-    return res.status(400).json({
-      error:
-        "name already exists in the phonebook, please use a different name",
-    });
-  }
-  const person = new Person({
+  const person = {
     name: body.name,
     number: body.number,
-  });
-  person.save().then((savedPerson) => {
-    res.json(savedPerson);
-  });
+  };
+  Person.findByIdAndUpdate(id, person, { new: true })
+    .then((updatedPerson) => {
+      res.json(updatedPerson);
+    })
+    .catch((error) => {
+      return next(error);
+    });
 });
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
