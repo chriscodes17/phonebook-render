f49e49a11452968b147447c844b010830a660a2a
Chris Juarez <chrisjuarez@Chriss-MacBook-Air.local>
2023-04-23 14:39:45 -0700

    eslint fixes

--- models/person.js
+++ models/person.js
@@ -1,10 +1,10 @@
-require("dotenv").config();
-const mongoose = require("mongoose");
+require("dotenv").config()
+const mongoose = require("mongoose")
 
-const url = process.env.MONGODB_URL;
+const url = process.env.MONGODB_URL
 
-mongoose.set("strictQuery", false);
-mongoose.connect(url);
+mongoose.set("strictQuery", false)
+mongoose.connect(url)
 
 //Schema
 const personSchema = new mongoose.Schema({
@@ -17,20 +17,20 @@ const personSchema = new mongoose.Schema({
     minLength: 8,
     validate: {
       validator: (phoneNumber) => {
-        const regex = /^(0\d{1,2}-\d{7}|\d{3}-\d{8})$/;
-        return regex.test(phoneNumber);
+        const regex = /^(0\d{1,2}-\d{7}|\d{3}-\d{8})$/
+        return regex.test(phoneNumber)
       },
       message: (props) => `${props.value} is not a valid phone number`,
     },
   },
-});
+})
 
 personSchema.set("toJSON", {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-module.exports = mongoose.model("Person", personSchema);
+module.exports = mongoose.model("Person", personSchema)
